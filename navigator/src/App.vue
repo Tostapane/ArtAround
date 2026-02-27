b19b0aca2d3b3f2ece9834fd925d09a0504ebdc0
Tostapane <79147922+Tostapane@users.noreply.github.com>
2026-02-27 15:02:24 +0100

    vue start

---
+++ navigator/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import DropDownMenu from './components/DropDownMenu.vue'
+</script>
+
+<template>
+  <main class="p-8"> <DropDownMenu />
+  </main>
+</template>
+
+<style scoped></style>
